db2b202047df1eb09988d2928314842caf7f0bd8
Jessica Nierth <jnierth@web.de>
2018-12-24 00:45:34 +1100

    feat (Added feature to control whether the outcome should be animated or not)

--- circular-progress-bar.js
+++ circular-progress-bar.js
@@ -5,29 +5,77 @@
  * @date: 12/23/2018
  */
 (function () {
+
     // Get all circles
     var circles = document.querySelectorAll('.circle');
 
+    // Initialize all circles
     for (var i = 0, len = circles.length; i < len; i++) {
         var circle = circles[i],
-            percent = circle.getAttribute('data-percentage');
+            speed = circle.getAttribute('data-anim-speed'),
+            percent = circle.getAttribute('data-percentage'),
+            radius = circle.getAttribute('data-radius'),
+            circumference = radius * 2 * Math.PI;
 
-        setProgress(circle, percent);
+        var tpl = createTemplate(radius);
+
+        addTemplateToDom(circle, tpl);
+        for (var i = 0; i <= percent; i++) {
+            if (speed) {
+                (function (l) {
+                    setTimeout(function () {
+                        setProgress(circle, l, radius, circumference);
+                    }, speed * l);
+                }(i));
+            } else {
+                setProgress(circle, i, radius, circumference);
+            }
+        }
     }
 
-    function setProgress(circle,percent) {
+    function setProgress(circle, percent, radius, circumference) {
 
-        var radius = 54,
-            circumference = radius * 2 * Math.PI,
-            offset,
+        var offset,
             text;
 
-        circle.style.strokeDasharray = `${circumference} ${circumference}`;
+        circle.style.strokeDasharray = circumference + " " + circumference;
 
         offset = circumference - percent / 100 * circumference;
         circle.style.strokeDashoffset = offset;
 
         text = circle.querySelector('.circle-progress-value');
         text.innerHTML = percent + '%';
+    }
+
+    function createTemplate(radius) {
+
+        return '<svg class="progress-ring"' +
+            'preserveAspectRatio="xMinYMin meet"' +
+            'viewBox="0 0 140 140">' +
+            '<defs>' +
+            '<linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">' +
+            '<stop offset="0%" stop-color="#00bc9b"/>' +
+            '<stop offset="100%" stop-color="#5eaefd"/>' +
+            '</linearGradient>' +
+            '</defs>' +
+            '<text class="circle-progress-value" x="50%" y="50%" text-anchor="middle" dominant-baseline="middle"></text>' +
+            '<circle class="progress-circle-inactive"' +
+            'stroke="white"' +
+            'stroke-width="4"' +
+            'fill="transparent"' +
+            'r="' + radius + '"' +
+            'cx="70"' +
+            'cy="70"/>' +
+            '<circle class="progress-circle-active"' +
+            'stroke="url(#gradient)"' +
+            'fill="transparent"' +
+            'r="' + radius + '"' +
+            'cx="70"' +
+            'cy="70"/>' +
+            '</svg>'
+    }
+
+    function addTemplateToDom(dom, tpl) {
+        dom.innerHTML = tpl;
     }
 }());
